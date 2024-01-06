de2848e03e912a93b74c9b076fbbd99ac66206f9
pRiYankA-2212 <145449555+pRiYankA-2212@users.noreply.github.com>
2024-01-06 23:39:57 +0530

    Add files via upload

---
+++ Variable.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a=30;
+    printf("%d",a);
+    //updation on variable
+    a=a+10;
+    printf("\n%d",a);
+    //Arthmatic operation (-,+,/,*,%)
+    int x=10;
+    int y=20;
+    printf("\n%d",x-y);
+
+    return 0;
+}
---
+++ input.c
@@ -0,0 +1,10 @@
+//taking input by user.
+#include<stdio.h>
+int main(){
+    float radius;
+    printf("enter radius :");
+    scanf("%f",&radius);   //input
+    float pi=3.14;
+     float areaoFcircle=pi*radius*radius;
+     printf("Areaofredius %f=" ,areaoFcircle);
+}
